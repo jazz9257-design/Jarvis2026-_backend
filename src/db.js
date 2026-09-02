2990673ee897e36bd43e6dbe6d060e4eb8fca12a
jazz9257-design <jazz9257@gmail.com>
2026-09-01 19:43:49 -0500

    Add Postgres connection helper

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+export function createPool() {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) {
+    throw new Error('DATABASE_URL is required');
+  }
+  return new Pool({ connectionString, ssl: connectionString.includes('localhost') ? false : { rejectUnauthorized: false } });
+}
